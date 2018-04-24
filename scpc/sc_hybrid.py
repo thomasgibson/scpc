0abe7368c067562e92bf10f58c7fda3de7e4c0f4
Thomas Gibson <t.gibson15@imperial.ac.uk>
2018-04-24 19:30:16 +0100

    Cached slate expressions

--- scpc/sc_hybrid.py
+++ scpc/sc_hybrid.py
@@ -2,6 +2,7 @@ from firedrake.matrix_free.preconditioners import PCBase
 from firedrake.matrix_free.operators import ImplicitMatrixContext
 from firedrake.petsc import PETSc
 from firedrake.slate.slate import Tensor, AssembledVector
+from firedrake.utils import cached_property
 from pyop2.profiling import timed_region, timed_function
 
 
@@ -49,6 +50,82 @@ class HybridSCPC(PCBase):
         # TODO: Clean this up
         assert W[2].ufl_element().family() == "HDiv Trace"
 
+        # Extract trace space
+        T = W[2]
+
+        # Need to duplicate a trace space which is NOT
+        # associated with a subspace of a mixed space.
+        Tr = FunctionSpace(T.mesh(), T.ufl_element())
+        bcs = []
+        cxt_bcs = self.cxt.row_bcs
+        for bc in cxt_bcs:
+            assert bc.function_space() == T, (
+                "BCs should be imposing vanishing conditions on traces"
+            )
+            if isinstance(bc.function_arg, Function):
+                bc_arg = interpolate(bc.function_arg, Tr)
+            else:
+                # Constants don't need to be interpolated
+                bc_arg = bc.function_arg
+            bcs.append(DirichletBC(Tr, bc_arg, bc.sub_domain))
+
+        mat_type = PETSc.Options().getString(prefix + "mat_type", "aij")
+
+        self.r_lambda = Function(T)
+        self.residual = Function(W)
+        self.solution = Function(W)
+
+        # Perform symbolics only once
+        S_expr, r_lambda_expr, u_h_expr, q_h_expr = self._slate_expressions
+
+        self.S = allocate_matrix(S_expr,
+                                 bcs=bcs,
+                                 form_compiler_parameters=self.cxt.fc_params,
+                                 mat_type=mat_type)
+        self._assemble_S = create_assembly_callable(
+            S_expr,
+            tensor=self.S,
+            bcs=bcs,
+            form_compiler_parameters=self.cxt.fc_params,
+            mat_type=mat_type)
+
+        self._assemble_S()
+        self.S.force_evaluation()
+        Smat = self.S.petscmat
+
+        # Set up ksp for the trace problem
+        trace_ksp = PETSc.KSP().create(comm=pc.comm)
+        trace_ksp.setOptionsPrefix(prefix)
+        trace_ksp.setOperators(Smat)
+        trace_ksp.setUp()
+        trace_ksp.setFromOptions()
+        self.trace_ksp = trace_ksp
+
+        self._assemble_Srhs = create_assembly_callable(
+            r_lambda_expr,
+            tensor=self.r_lambda,
+            form_compiler_parameters=self.cxt.fc_params)
+
+        q_h, u_h, lambda_h = self.solution.split()
+
+        # Assemble u_h using lambda_h
+        self._assemble_u = create_assembly_callable(
+            u_h_expr,
+            tensor=u_h,
+            form_compiler_parameters=self.cxt.fc_params)
+
+        # Recover q_h using both u_h and lambda_h
+        self._assemble_q = create_assembly_callable(
+            q_h_expr,
+            tensor=q_h,
+            form_compiler_parameters=self.cxt.fc_params)
+
+    @cached_property
+    def _slate_expressions(self):
+        """Returns all the relevant Slate expressions
+        for the static condensation and local recovery
+        procedures.
+        """
         # This operator has the form:
         # | A  B  C |
         # | D  E  F |
@@ -85,45 +162,17 @@ class HybridSCPC(PCBase):
         # Schur complement for traces
         S = J - L * M.inv * K
 
-        # Extract trace space
-        T = W[2]
+        # Create mixed function for residual computation.
+        # This projects the non-trace residual bits into
+        # the trace space:
+        # -L * M.inv * | v1 v2 |^T
+        R = AssembledVector(self.residual)
+        v1v2 = R.block(((0, 1),))
+        v3 = R.block((2,))
+        r_lambda = v3 - L * M.inv * v1v2
 
-        # Need to duplicate a trace space which is NOT
-        # associated with a subspace of a mixed space.
-        Tr = FunctionSpace(T.mesh(), T.ufl_element())
-        bcs = []
-        cxt_bcs = self.cxt.row_bcs
-        for bc in cxt_bcs:
-            assert bc.function_space() == T, (
-                "BCs should be imposing vanishing conditions on traces"
-            )
-            if isinstance(bc.function_arg, Function):
-                bc_arg = interpolate(bc.function_arg, Tr)
-            else:
-                # Constants don't need to be interpolated
-                bc_arg = bc.function_arg
-            bcs.append(DirichletBC(Tr, bc_arg, bc.sub_domain))
-
-        self.S = allocate_matrix(S,
-                                 bcs=bcs,
-                                 form_compiler_parameters=self.cxt.fc_params)
-        self._assemble_S = create_assembly_callable(
-            S,
-            tensor=self.S,
-            bcs=bcs,
-            form_compiler_parameters=self.cxt.fc_params)
-
-        self._assemble_S()
-        self.S.force_evaluation()
-        Smat = self.S.petscmat
-
-        # Set up ksp for the trace problem
-        trace_ksp = PETSc.KSP().create(comm=pc.comm)
-        trace_ksp.setOptionsPrefix(prefix)
-        trace_ksp.setOperators(Smat)
-        trace_ksp.setUp()
-        trace_ksp.setFromOptions()
-        self.trace_ksp = trace_ksp
+        # Reconstruction expressions
+        q_h, u_h, lambda_h = self.solution.split()
 
         # Local tensors needed for reconstruction
         A = O.block((0, 0))
@@ -135,45 +184,21 @@ class HybridSCPC(PCBase):
         Se = E - D * A.inv * B
         Sf = F - D * A.inv * C
 
-        # Expression for RHS assembly
-        # Set up the functions for trace solution and
-        # the residual for the trace system
-        self.r_lambda = Function(T)
-        self.residual = Function(W)
-        self.r_lambda_thunk = Function(T)
         v1, v2, v3 = self.residual.split()
 
-        # Create mixed function for residual computation.
-        # This projects the non-trace residual bits into
-        # the trace space:
-        # -L * M.inv * | v1 v2 |^T
-        R = AssembledVector(self.residual)
-        v1v2 = R.block(((0, 1),))
-        v3 = R.block((2,))
-        r_lambda_thunk = v3 - L * M.inv * v1v2
-        self._assemble_Srhs_thunk = create_assembly_callable(
-            r_lambda_thunk,
-            tensor=self.r_lambda_thunk,
-            form_compiler_parameters=self.cxt.fc_params)
+        # Solve locally using Cholesky factorizations
+        # (Se and A are symmetric positive definite)
+        u_h_expr = Se.solve(AssembledVector(v2) -
+                            D * A.inv * AssembledVector(v1) -
+                            Sf * AssembledVector(lambda_h),
+                            decomposition="LLT")
 
-        self.solution = Function(W)
-        q_h, u_h, lambda_h = self.solution.split()
+        q_h_expr = A.solve(AssembledVector(v1) -
+                           B * AssembledVector(u_h) -
+                           C * AssembledVector(lambda_h),
+                           decomposition="LLT")
 
-        # Assemble u_h using lambda_h
-        self._assemble_u = create_assembly_callable(
-            Se.inv * (AssembledVector(v2) -
-                      D * A.inv * AssembledVector(v1) -
-                      Sf * AssembledVector(lambda_h)),
-            tensor=u_h,
-            form_compiler_parameters=self.cxt.fc_params)
-
-        # Recover q_h using both u_h and lambda_h
-        self._assemble_q = create_assembly_callable(
-            A.inv * (AssembledVector(v1) -
-                     B * AssembledVector(u_h) -
-                     C * AssembledVector(lambda_h)),
-            tensor=q_h,
-            form_compiler_parameters=self.cxt.fc_params)
+        return (S, r_lambda, u_h_expr, q_h_expr)
 
     @timed_function("HybridSCUpdate")
     def update(self, pc):
@@ -195,11 +220,11 @@ class HybridSCPC(PCBase):
 
         with timed_region("HybridSCRHS"):
             # Now assemble residual for the reduced problem
-            self._assemble_Srhs_thunk()
+            self._assemble_Srhs()
 
         with timed_region("HybridSCSolve"):
             # Solve the system for the Lagrange multipliers
-            with self.r_lambda_thunk.dat.vec_ro as b:
+            with self.r_lambda.dat.vec_ro as b:
                 if self.trace_ksp.getInitialGuessNonzero():
                     acc = self.solution.split()[2].dat.vec
                 else:

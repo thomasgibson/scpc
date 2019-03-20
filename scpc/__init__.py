05f47b12412e3bf64b71bc6b5f6b437686fd739c
Thomas Gibson <t.gibson15@imperial.ac.uk>
2019-03-20 19:04:00 +0000

    Add vertical hybridization PC

--- scpc/__init__.py
+++ scpc/__init__.py
@@ -1,3 +1,4 @@
-from scpc.sccg import SCCG                    # noqa: F401
-from scpc.hybridmixed import HybridizationPC  # noqa: F401
-from scpc.sc_hybrid import HybridSCPC         # noqa: F401
+from scpc.sccg import SCCG                                       # noqa: F401
+from scpc.hybridmixed import HybridizationPC                     # noqa: F401
+from scpc.sc_hybrid import HybridSCPC                            # noqa: F401
+from scpc.vertical_hybridization import VerticalHybridizationPC  # noqa: F401
